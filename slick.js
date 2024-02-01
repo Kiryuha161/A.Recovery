8784141dc3597a9c074f16f26904020bf3bd7e85
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2024-02-01 23:25:47 +0300

    Слайдер для мобилки, блоки для компа

--- slick.js
+++ slick.js
@@ -1,20 +1,21 @@
 $(document).ready(function () {
-    $('.multiple-items').slick({
+    $('.main-multiple-items').slick({
         infinite: true,
         autoplay: true,
         slidesToShow: 3,
         dots: true,
         slidesToScroll: 1,
+        centerMode: true,
         responsive: [
             {
-                breakpoint: 1000,
+                breakpoint: 1200,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1
                 }
             },
             {
-                breakpoint: 768,
+                breakpoint: 1000,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
@@ -24,29 +25,102 @@ $(document).ready(function () {
     })
 });
 
+$(document).ready(function () {
+    $('.extra-multiple-items').slick({
+        infinite: true,
+        autoplay: true,
+        slidesToShow: 3,
+        dots: true,
+        slidesToScroll: 1,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    })
+});
 
-/* $('.multiple-items').slick({
-      infinite: true,
-      autoplay: true,
-      slidesToShow: 3,
-      dots: true,
-      slidesToScroll: 1,
-      responsive: [
-          {
-              breakpoint: 1000,
-              settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 1
-              }
-          },
-          {
-              breakpoint: 768,
-              settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
-              }
-          }
-      ]
+$(document).ready(function () {
+    $('.multiple-items').slick({
+        infinite: true,
+        autoplay: true,
+        slidesToShow: 3,
+        dots: true,
+        slidesToScroll: 1,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    })
+});
+
+/* $isSliderOn = false;
+function greenSlider(){    
+    if($(window).width() < 991){
+        if(!$isSliderOn){
+            $(".multiple-items").slick({
+                dots: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                //autoplay: true
+            });
+            $isSliderOn = true;
+        }
+    } else if($(window).width() > 992){
+        if($isSliderOn){
+            $('.multiple-items').slick('unslick');
+            $isSliderOn = false;
+        }
+    }
+};
+
+$(document).ready(function(){
+    greenSlider();
+});
+$(window).on('resize', function(){
+     greenSlider();
+}); */
+
+
+/* $(document).ready(function () {
+    $('.multiple-items').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToScroll: 1,
+        variableWidth: true,
+        mobileFirst: true,
+        responsive: [{
+            breakpoint: 992,
+            settings: 'unslick'
+        }]
+    })
+});
 
       $('.multiple-items').slick({
       arrows: false,
